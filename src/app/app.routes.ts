0f29faba330aa6ebe8d11370b538a0db9708fa51
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2026-01-17 11:54:16 +0530

    registration functionality for doctor and patient

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { SignUp } from './components/log-in/sign-up/sign-up'; 
+import { LogIn } from './components/log-in/log-in';
+export const routes: Routes = [
+  { path: '', component: LogIn }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LogIn } from './components/log-in/log-in';
+import { Sign } from 'crypto';
+import { SignUp } from './components/log-in/sign-up/sign-up';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LogIn],
+  imports: [RouterOutlet, LogIn, SignUp],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
